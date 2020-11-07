d7bf23a3884da168e726dc4545ae966d88672d20
Luis A. Perez <lperezdude@gmail.com>
2020-11-07 13:28:50 -0500

    Took out auhtentication

--- elromco.php
+++ elromco.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Elromco - JS Form
 Description: This plug in adds short codes to your wordpress site used to display the Elromco Forms.
-Version: 1.0.9
+Version: 1.0.10
 Author: Luis Perez
 License: GPLv2 or later
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
@@ -35,7 +35,7 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 );
 
 //Optional: If you're using a private repository, specify the access token like this:
-//$myUpdateChecker->setAuthentication('b27e7b6d3306c11be37ac2a2a0cdd4a73dfcd83f');
+$myUpdateChecker->setAuthentication('b27e7b6d3306c11be37ac2a2a0cdd4a73dfcd83f');
 
 //Checking for Releases
 $myUpdateChecker->getVcsApi()->enableReleaseAssets();
