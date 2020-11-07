6144eec7fb345802ad53801de157f276ac0d6697
Luis A. Perez <lperezdude@gmail.com>
2020-11-07 13:37:49 -0500

    New Token

--- elromco.php
+++ elromco.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Elromco - JS Form
 Description: This plug in adds short codes to your wordpress site used to display the Elromco Forms.
-Version: 1.0.14
+Version: 1.0.15
 Author: Luis Perez
 License: GPLv2 or later
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
@@ -35,7 +35,7 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 );
 
 //Optional: If you're using a private repository, specify the access token like this:
-$myUpdateChecker->setAuthentication('b27e7b6d3306c11be37ac2a2a0cdd4a73dfcd83f');
+$myUpdateChecker->setAuthentication('dace0c146f019b6a31bddb610b0140a012069b17');
 
 //Checking for Releases
 $myUpdateChecker->getVcsApi()->enableReleaseAssets();
